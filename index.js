5558bdc7d7fb30b6d0210b4b73836fa3da52150d
Yusuf Büyüktorun <ybuyuktoruntest@gmail.com>
2022-09-21 17:39:58 +0300

    deploy

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const { circleArea,circleCircumference } = require("./circle.js");
+circleArea(5);
+circleCircumference(5);
+
+
+// import { circleArea,circleCircumference } from "./circle.js";
+// circleArea(5);
+// circleCircumference (5);
